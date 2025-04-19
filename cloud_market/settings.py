f6e1ac11e35eda3f530ed78a5628f51d8d102b6b
woyeta <148470837+woyeta@users.noreply.github.com>
2025-04-19 20:52:24 +0530

    deployment support

--- cloud_market/settings.py
+++ cloud_market/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
